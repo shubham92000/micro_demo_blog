4c2790e64d3bedf21a92d3d0848d77c2e8b736a0
Shubham Singh <shubham.singh@tatadigital.com>
2022-10-18 20:20:22 +0530

    updated localhost to clusterIp

--- event-bus/index.js
+++ event-bus/index.js
@@ -8,30 +8,31 @@ app.use(bodyParser.json());
 const events = [];
 
 app.post('/events', (req, res) => {
-  const event = req.body;
+	const event = req.body;
 
-  events.push(event);
+	events.push(event);
+	console.log(event);
 
-  axios
-    .post('http://localhost:4000/events', event)
-    .catch((err) => console.log(err));
-  axios
-    .post('http://localhost:4001/events', event)
-    .catch((err) => console.log(err));
-  axios
-    .post('http://localhost:4002/events', event)
-    .catch((err) => console.log(err));
-  axios
-    .post('http://localhost:4003/events', event)
-    .catch((err) => console.log(err));
+	axios
+		.post('http://posts-clusterip-srv:4000/events', event)
+		.catch((err) => console.log(err));
+	axios
+		.post('http://comments-clusterip-srv:4001/events', event)
+		.catch((err) => console.log(err));
+	axios
+		.post('http://query-clusterip-srv:4002/events', event)
+		.catch((err) => console.log(err));
+	axios
+		.post('http://moderation-clusterip-srv:4003/events', event)
+		.catch((err) => console.log(err));
 
-  res.send({ status: 'OK' });
+	res.send({ status: 'OK' });
 });
 
 app.get('/events', (req, res) => {
-  res.send(events);
+	res.send(events);
 });
 
 app.listen(4005, () => {
-  console.log('listening on port 4005 for event bus...');
+	console.log('listening on port 4005 for event bus...');
 });
--- posts/index.js
+++ posts/index.js
@@ -11,36 +11,36 @@ app.use(cors());
 const posts = {};
 
 app.get('/posts', (req, res) => {
-  res.send(posts);
+	res.send(posts);
 });
 
 app.post('/posts', async (req, res) => {
-  const id = randomBytes(4).toString('hex');
-  const { title } = req.body;
+	const id = randomBytes(4).toString('hex');
+	const { title } = req.body;
 
-  posts[id] = {
-    id,
-    title,
-  };
+	posts[id] = {
+		id,
+		title,
+	};
 
-  await axios.post('http://localhost:4005/events', {
-    type: 'PostCreated',
-    data: {
-      id,
-      title,
-    },
-  });
+	await axios.post('http://event-bus-clusterip-srv:4005/events', {
+		type: 'PostCreated',
+		data: {
+			id,
+			title,
+		},
+	});
 
-  res.status(201).send(posts[id]);
+	res.status(201).send(posts[id]);
 });
 
 app.post('/events', (req, res) => {
-  console.log(`Received event ${req.body.type}`);
-  res.send({});
+	console.log(`Received event ${req.body.type}`);
+	res.send({});
 });
 
 app.listen(4000, () => {
-  console.log(`listening on port 4000 for posts service...`);
+	console.log(`listening on port 4000 for posts service...`);
 });
 
 // docker run 711b0708d62eb357a34e64b21aea30eb0723fe2b2392292282dfd12ed25f5ce5
